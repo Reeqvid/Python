d917f77aa3368f99d8bd57455d336154e680eda0
Reeqvid <Reeqvidcub@gmail.com>
2021-12-05 14:34:17 +0300

    HW 1

---
+++ project/first/HW_1_python.py
@@ -0,0 +1,28 @@
+a = "OLeg"
+print(a, type(a))
+b = 13
+print(b, type(b))
+c = 13.3
+print(c, type(c))
+d = b"sd123asddasd"
+print(d, type(d))
+f = {"ghbdtn", "Kak dela", "normal"}
+print(f, type(f))
+g = ("Privet", "Poka", "Spisok")
+print(g, type(g))
+h = ["a", 13, "C", "array"]
+print(h, type(h))
+z = ["Array", "List"]
+fz = frozenset(z)
+print(z, type(fz))
+x = {"Name": "Oleg", "age": "24"}
+print(x, type(x))
+
+
+qw = "Oleg "
+qe = "Tsaryuk"
+qa = qw + qe
+print(qa)
+
+print(str(b), qw)
+print("String + integer --", str(b) + " " + qw)
